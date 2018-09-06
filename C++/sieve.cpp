d878601e28d4c610183c3e9f61bc748f13ddba6f
Shubham Jain <bugdivine@gmail.com>
2018-09-06 23:21:31 +0530

    Added documentation and assertions for sieve

--- C++/sieve.cpp
+++ C++/sieve.cpp
@@ -2,27 +2,44 @@
 #include <cstdio>
 #include <cmath>
 #include <cstdlib>
+#include <cassert>
 using namespace std;
 
-int *sieve(int n)
+/**
+ * Sieve of Eratosthenses
+ * https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+ * n: Number till which we want to find if a number is prime or not
+ * returns: Boolean array. 0 => Prime, 1 => Composite
+ */
+bool *sieve(int n)
 {
-	int *result = (int *)malloc((n+1)*sizeof(int));
+	assert(n >= 1);
+	bool *result = (bool *)malloc((n+1)*sizeof(bool));
+
+	// Mark all even numbers as composite and all odd numbers as prime
 	for (int i = 0; i < n+1; ++i)
-	{
-		result[i]=(i+1)%2;
-	}
+		result[i] = (i+1)%2;
+
+	// Cases which fail the generalization that all even numbers are 
+	// composite
 	result[1] = 1;
-	result[2] = 0;
+	if (n >= 2)
+		result[2] = 0;
+
 	double x = sqrt(n);
-	for (int i = 3; i <= x; ++i)
+	// Iterate over all the prime marked numbers and
+	// mark their multiples as composite
+	// All numbers greater than x and less than n are already marked as
+	// prime or composite as multiples of lesser numbers than x
+	for (int i = 3; i <= x; i+=2)
 	{
 		if (result[i]==0)
 		{
-			int j=i*i;
-			while (j<=n)
+			int j = i*i;
+			while (j <= n)
 			{
-				result[j]=1;
-				j+=i;
+				result[j] = 1;
+				j += i;
 			}
 		}
 	}
